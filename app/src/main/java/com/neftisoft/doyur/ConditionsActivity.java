ec4f498c1ce0b173e769e12d6cbf39db0d642e5d
Samet Tüzemen <justmansam@gmail.com>
2022-03-31 09:35:56 +0300

    Create ConditionsActivity.java

---
+++ app/src/main/java/com/neftisoft/doyur/ConditionsActivity.java
@@ -0,0 +1,36 @@
+package com.neftisoft.doyur;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class ConditionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conditions);
+
+        // To change the status bar color!
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = ConditionsActivity.this.getWindow();
+            // clear FLAG_TRANSLUCENT_STATUS flag:
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            // finally change the color
+            window.setStatusBarColor(ContextCompat.getColor(ConditionsActivity.this,R.color.colorNavigationBar));
+        }
+    }
+    /*
+    TO GET BACK !!!
+     */
+    public void getBack(View view) {
+        onBackPressed();
+    }
+}
